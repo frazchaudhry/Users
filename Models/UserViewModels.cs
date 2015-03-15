758b344b411ec700cab4db3ee4a4753c4e9ea1d5
chilli_501 <fraz501@hotmail.com>
2015-03-15 18:14:38 -0500

    Auth

--- Models/UserViewModels.cs
+++ Models/UserViewModels.cs
@@ -17,4 +17,13 @@ namespace Users.Models
         [Required]
         public string Password { get; set; }
     }
+
+    public class LoginModel
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+    }
 }
